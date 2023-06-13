381d4b012d4522aac53fc4c4bd1767a08628550d
Aleksandr Shunko <shunko1999@gmail.com>
2023-06-13 16:58:52 +0300

    feat: create components
    BoardComponents and CellComponents
    Add base styles for them

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BoardComponent from './components/BoardComponent';
 
 function App() {
   return (
-    <div>
-     work
+    <div className='app'>
+     <BoardComponent></BoardComponent>
     </div>
   );
 }
