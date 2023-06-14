2034ebf8e0ae582cfe2815dd9cdc65d85baea3b4
Aleksandr Shunko <shunko1999@gmail.com>
2023-06-14 18:47:47 +0300

    feat: Add all figures to board

--- my-app/src/model/Board.ts
+++ my-app/src/model/Board.ts
@@ -38,19 +38,34 @@ export class Board {
     }
 
     private addKings () {
-
+        new King(Colors.BLACK, this.getCell(4, 0))
+        new King(Colors.WHITE, this.getCell(4, 7))
     }
 
     private addQueens() {
-
+        new Queen(Colors.BLACK, this.getCell(3, 0))
+        new Queen(Colors.WHITE, this.getCell(3, 7))
     }
 
     private addBishops() {
-
+        new Bishop(Colors.BLACK, this.getCell(2, 0))
+        new Bishop(Colors.BLACK, this.getCell(5, 0))
+        new Bishop(Colors.WHITE, this.getCell(2, 7))
+        new Bishop(Colors.WHITE, this.getCell(5, 7))
     }
 
     private addKnights() {
-
+        new Knight(Colors.BLACK, this.getCell(1, 0))
+        new Knight(Colors.BLACK, this.getCell(6, 0))
+        new Knight(Colors.WHITE, this.getCell(1, 7))
+        new Knight(Colors.WHITE, this.getCell(6, 7))
+    
+    }
+    private addRooks() {
+        new Rook(Colors.BLACK, this.getCell(0, 0))
+        new Rook(Colors.BLACK, this.getCell(7, 0))
+        new Rook(Colors.WHITE, this.getCell(0, 7))
+        new Rook(Colors.WHITE, this.getCell(7, 7))
     }
 
     public addFigures() {
@@ -59,6 +74,6 @@ this.addKnights()
 this.addKings()
 this.addBishops()
 this.addQueens()
-
+this.addRooks()
     }
 }
