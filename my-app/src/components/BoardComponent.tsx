ee602a48f2215774d41b153c7ce2f647cac95d08
Aleksandr Shunko <shunko1999@gmail.com>
2023-06-15 15:22:05 +0300

    feat: Cell avtive only when have figure
    The click only works where there is a figure

--- my-app/src/components/BoardComponent.tsx
+++ my-app/src/components/BoardComponent.tsx
@@ -13,7 +13,9 @@ const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
     const [selectedCell, setSelectedCell] = useState<Cell | null> (null);
 
 function click (cell:Cell) {
-setSelectedCell(cell);
+    if (cell.figure) {
+        setSelectedCell(cell);
+    }
 }
 
     return (
