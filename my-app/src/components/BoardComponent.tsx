c53c86a26925b890668b4a65f03de91afa52f5a9
Aleksandr Shunko <shunko1999@gmail.com>
2023-06-15 14:48:13 +0300

    feat: add click to cell
    Now you can click to cell and change color

--- my-app/src/components/BoardComponent.tsx
+++ my-app/src/components/BoardComponent.tsx
@@ -1,4 +1,4 @@
-import React, {FC} from "react";
+import React, {FC, useState} from "react";
 import { Board } from "../model/Board";
 import CellComponent from "./CellComponent";
 import { Cell } from "../model/Cell";
@@ -10,14 +10,22 @@ interface BoardProps {
 }
 
 const BoardComponent: FC<BoardProps> = ({board, setBoard}) => {
+    const [selectedCell, setSelectedCell] = useState<Cell | null> (null);
+
+function click (cell:Cell) {
+setSelectedCell(cell);
+}
+
     return (
         <div className="board">
             {board.cells.map((row: Cell[], index: number) =>
             <React.Fragment key={index}>
                 {row.map(cell =>
                 <CellComponent
+                click={click}
                 cell={cell}
                 key={cell.id}
+                selected = {cell.x === selectedCell?.x && cell.y === selectedCell.y}
                 />
                 )}
             </React.Fragment>
